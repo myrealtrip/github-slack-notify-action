a8d68430213f7f1d882edebd250a5f1ffd364909
geonwoo.jeong <doscmdev@gmail.com>
2021-11-02 13:56:37 +0900

    refactor: canary parser

--- src/utils/parseCanaryVersion.ts
+++ src/utils/parseCanaryVersion.ts
@@ -16,12 +16,9 @@ export function parseCanaryVersion(body: string) {
   const versionNote = parse[0];
 
   const startIndex = findStringLastIndex(versionNote, matchString.start);
-  const endIndex =
-    findStringLastIndex(versionNote, matchString.end) -
-    startIndex -
-    matchString.end.length;
+  const endIndex = findStringLastIndex(versionNote, matchString.end);
 
-  const version = versionNote.substr(startIndex, endIndex);
+  const version = versionNote.substr(startIndex, endIndex).replace(" Done", "");
 
   const markdown = `\`\`\`// npm\nnpm install ${PACKAGE_NAME}@${version}\n\n// yarn\nyarn add ${PACKAGE_NAME}@${version}\`\`\``;
 
