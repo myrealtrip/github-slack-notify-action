db20d0a35401909cc93f11098e8e1859065c825d
zi-gae <doscmdev@gmail.com>
2022-04-07 12:01:24 +0900

    fix: install node fetch

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,5 +1,6 @@
 import { User } from "models/user";
 import { USER_INFO_URL } from "./input";
+import fetch from "node-fetch";
 
 export async function fetchDevelopers() {
   const response = await fetch(USER_INFO_URL);
